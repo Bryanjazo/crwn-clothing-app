97dc5e117558506db724258fd7c0f522d26a1a63
Bryan Jazo <Bryanjazo88@gmail.com>
2022-11-30 21:06:13 -0800

    added categories component. also placed a directory folder to enable the routing of the pgaes

---
+++ src/components/directory/Directory.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import Categories from "../categories/Categories";
+export default function Directory({ categories }) {
+  return (
+    <div className="categories-container">
+      {categories.map((category) => (
+        <Categories key={category.id} categories={category} />
+      ))}
+    </div>
+  );
+}
